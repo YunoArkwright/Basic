958659899deae021aafd3f8b253ed6a58c7fbaea
YunoArkwright <requem07@mail.ru>
2022-07-27 23:02:52 +0800

    Some comments added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,11 +10,16 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
+        //Making url DB
         String url = "jdbc:sqlite:C:/Dev/sqlite/westeros.db";
-
+        
+        //Making source, setting url in there
         SQLiteDataSource dataSource = new SQLiteDataSource();
         dataSource.setUrl(url);
+        
+        System.out.println("Just checking");
 
+        //Establishing connection
         try (Connection con = dataSource.getConnection()) {
             // Statement creation
             try (Statement statement = con.createStatement()) {
@@ -22,7 +27,7 @@ public class Main {
                 statement.executeUpdate("CREATE TABLE IF NOT EXISTS HOUSES(" +
                         "id INTEGER PRIMARY KEY," +
                         "name TEXT NOT NULL," +
-                        "words TEXT NOT NULL)");
+                        "words TEXT NOT NULL)");  
             } catch (SQLException e) {
                 e.printStackTrace();
             }
